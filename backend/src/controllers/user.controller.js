e68308649821c03845e33a5324d033a030b548a7
Omkar09 <143746159+Omkarlaxmansurvase@users.noreply.github.com>
2026-02-03 00:33:05 +0530

    login and register routes

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,60 @@
+import {user} from "../models/users.models.js";
+import bcrypt,{hash} from "bcrypt";
+import httpStatus from "http-status";
+
+// User Registration Controller
+const register = async (req , res)=>{
+    const {name,username,email,password} = req.body;
+
+    try {
+            const existingUser = await user.findOne({username});
+            if (existingUser){
+                return res.status(httpStatus.FOUND).json({message:"User already exists"});
+            }
+
+            const hashedPassword = await bcrypt.hash(password,10);
+
+            const newUser = new user({
+                name:name,
+                username:username,
+                email:email,
+                password:hashedPassword,
+            })
+            await newUser.save();
+            return res.status(httpStatus.CREATED).json({message:"User registered successfully"});
+        }
+    catch(error){
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:"Error registering user", error:error.message});
+    }
+}
+// User Login Controller
+const login = async (req , res) =>{
+    const {username,password} = req.body;
+    
+    if (!username || !password){
+        return res.status(httpStatus.BAD_REQUEST).json({message:"Username and password are required"});
+    }
+    
+    try {
+        const user  = await user.find({username});
+        if (!user){
+            return res.status(httpStatus.NOT_FOUND).json({message:"User not found"});
+        }
+
+        if(bcrypt.compare(passsword,user.password)){
+            let token = crypto.randomBytes(20).toString("hex");
+
+            user.token = token;
+            await user.save();
+            return res.status(httpStatus.OK).json({token:token})
+        }
+
+    }
+    
+
+    catch(e){
+        return res.status(500).json({message:"smthng went wrong"})
+    }
+}
+
+export {register,login};
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import {route} from 'express';
+import {register,login } from "../controllers/user.controller.js"
 
 const router = route();
 
-router.route('/login')
-router.route('/register')
+router.route('/login').post(login);
+router.route('/register').post(register);
 router.route('/users')
 router.route('/users')
 
