8e2ae9b3147aee0b489225366fa10d07c409387a
Omkar09 <143746159+Omkarlaxmansurvase@users.noreply.github.com>
2026-02-06 11:19:04 +0530

    socket connection

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,7 +1,34 @@
 import { Server } from "socket.io";
 
+let connections = {};
+let messages = {};
+let timeOnline = {};
+
 export const connectToSocket = (server) => {
     const io = new Server(server);
+
+
+    io.on("connection", (socket) => {
+
+        // Handle joining a call
+        socket.on("join-call",(path)=>{
+
+            if(connections[path]===undefined){
+                
+            }
+
+        })
+
+        // Handle incoming signals
+        socket.on("signal",(toId,message)=>{
+            io.to(toId).emit("signal",socket.id,message);
+        })
+
+        // Handle disconnection
+        socket.on("disconnect",()=>{
+
+        });
+    });
 
     return io;
 }
