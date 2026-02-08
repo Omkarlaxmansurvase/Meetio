f049963b93afaf6e22ec9476e782d2a3c6966a1d
Omkar09 <143746159+Omkarlaxmansurvase@users.noreply.github.com>
2026-02-08 18:11:42 +0530

    join-call function

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -14,7 +14,21 @@ export const connectToSocket = (server) => {
         socket.on("join-call",(path)=>{
 
             if(connections[path]===undefined){
-                
+                connections[path]=[];
+
+            }
+            connections[path].push(socket.id);
+            timeOnline[socket.id]= new Date();
+
+            for(let a = 0; a<connections[path].length; i++){
+                io.to(connection[path][a]).emmit("user-joined",socket.id,connections[path]);
+            }
+            if(messages[path]!==undefined){
+                for(let a=0; a<messages[path].length;++a){
+                    io.to(socket.id).emmit("chat-message",messages[path][a]['data'],
+                    messages[path][a]['sender'],messages[path][a]['socket-id-sender']
+                    );
+                }
             }
 
         })
@@ -22,6 +36,11 @@ export const connectToSocket = (server) => {
         // Handle incoming signals
         socket.on("signal",(toId,message)=>{
             io.to(toId).emit("signal",socket.id,message);
+        })
+
+        // Handle chat messages
+        socket.on("chat-message",(data,sender)=>{
+            
         })
 
         // Handle disconnection
