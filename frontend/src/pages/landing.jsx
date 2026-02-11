2f686c4dbd2d92e5c44e76a50d434ae7b55a2581
Omkar09 <143746159+Omkarlaxmansurvase@users.noreply.github.com>
2026-02-11 20:47:18 +0530

    landing page implementation

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import "../App.css"
+import {Link} from "react-router-dom"
 
 export default function LandingPage() {
   return (
     <div className='landingPageContainer'>
         
         <nav>
-            <div></div>
+            <div className="navHeader">
+              <h2>Meetio</h2>
+            </div>
+            <div className="navList">
+              <p>Join as guest</p>
+              <p>Register</p>
+              <div role="button">
+                <p>Login</p>
+              </div>
+            </div>
             
         </nav>
+
+        <div className="landingMainContainer">
+          <div>
+            <h1><span style={{color: "#D97500"}}>connect</span> with your loved ones</h1>
+            <p>join a distance by meetio :D</p>
+            <div role='button'>
+              <Link to={"/home"}>Get Started</Link>
+            </div>
+          </div>
+          <div>
+
+            <img src="/meet.png" alt="" />
+          </div>
+        </div>
         
     </div>
   )
