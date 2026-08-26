dcfd354d5cc586ffed85e30fbc6527833c26b52e
Omkarlaxmansurvase <omkarsurvase2104@gmail.com>
2026-08-26 23:17:49 +0530

    fix production build

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,3 @@
 // let IS_PROD = true;
-const server =  
-    // "https://apnacollegebackend.onrender.com" :
-
-    "https://meetio-w29k.onrender.com"
-
-
+const server =  "https://meetio-w29k.onrender.com"
 export default server;
